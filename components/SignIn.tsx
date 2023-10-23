e2831ab1563b4713e91fda5890d26989d933df08
Saleh Ahmed <dev.salehahmed@gmail.com>
2023-10-23 05:52:52 +0500

    -signin.txs --callback-url-conditional

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -7,6 +7,9 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 function SignIn() {
+  const productionUrlRedirect = "https://daily-post-app.vercel.app/dashboard";
+  const developmentUrlRedirect = "http://localhost:3000/dashboard";
+
   return (
     <div className="main-tag flex flex-col justify-center items-center gap-5">
       <Button
@@ -15,7 +18,14 @@ function SignIn() {
         className="flex items-center gap-2"
         // sign in google callback
         onClick={() =>
-          signIn("google", { callbackUrl: "http://localhost:3000/dashboard" })
+          // signIn("google", { callbackUrl: "http://localhost:3000/dashboard" })
+          // signin with google and redirect to dashboard page after signin success if error redirect to signin page again if app is in production mode then redirect to production url else redirect to localhost url for development   mode process.env.NODE_ENV === "production" ? "https://next-auth-example.vercel.app/dashboard" : "http://localhost:3000/dashboard" if error redirect to signin page again
+          signIn("google", {
+            callbackUrl:
+              process.env.NODE_ENV === "production"
+                ? `${productionUrlRedirect}`
+                : `${developmentUrlRedirect}`,
+          })
         }
       >
         <span className="text-[20px]">
@@ -30,7 +40,12 @@ function SignIn() {
         className="flex items-center gap-2 "
         // sign in google callback
         onClick={() =>
-          signIn("github", { callbackUrl: "http://localhost:3000/dashboard" })
+          signIn("github", {
+            callbackUrl:
+              process.env.NODE_ENV === "production"
+                ? `${productionUrlRedirect}`
+                : `${developmentUrlRedirect}`,
+          })
         }
       >
         <span className="text-[20px]">
